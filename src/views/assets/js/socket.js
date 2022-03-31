6de860b0b3b4d2f9f69ea023546227bb184c23a9
Bryce <46429473+brycethedeveloper@users.noreply.github.com>
2022-03-31 18:42:04 -0400

    Delete src directory

--- src/views/assets/js/socket.js
+++
@@ -1,8 +0,0 @@
-const socket = io.connect("https://bhbotlist.xyz");
-
-socket.on('userCount', userCount => {
-let doc = document.getElementById('connectionCount');
-  if(doc) {
-    doc.innerHTML = userCount;
-  }
-})
